19d30f7d08fbcc66f95f437140e1136a92d090d6
Amit Kumar <amitkumar.developer1@gmail.com>
2021-01-29 00:24:51 +0530

    added file to store data

--- starting-with-express/models/product.js
+++ starting-with-express/models/product.js
@@ -1,15 +1,36 @@
-const products = [];
+const path = require('path');
+const fs = require('fs');
+const p = path.join(
+	path.dirname(process.mainModule.filename),
+	'data',
+	'products.json',
+);
+
+const getProductFromFile = (cb) => {
+	fs.readFile(p, (err, fileData) => {
+		if (err) {
+			cb([]);
+		} else {
+			cb(JSON.parse(fileData));
+		}
+	});
+};
 
 module.exports = class Product {
-    constructor(t) {
-        this.title = t;
-    }
+	constructor(t) {
+		this.title = t;
+	}
 
-    save() {
-        products.push(this);
-    }
+	save() {
+		getProductFromFile((products) => {
+			products.push(this);
+			fs.writeFile(p, JSON.stringify(products), (err) => {
+				if (err) throw err;
+			});
+		});
+	}
 
-    static fetchAll() {
-        return products;
-    }
-}
+	static fetchAll(cb) {
+		getProductFromFile(cb);
+	}
+};
